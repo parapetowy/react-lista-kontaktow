6929fffe7c060fc5e7752e006d36063b253e68c4
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-08-23 12:18:19 +0200

    contact list done

---
+++ components/Contact.js
@@ -0,0 +1,21 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', {className: 'contactItem'},
+        React.createElement('img', {
+          className: 'contactImage',
+          src: 'http://icons.iconarchive.com/icons/iynque/ios7-style/512/Contacts-icon.png'
+        }),
+        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+          this.props.item.email
+        )
+      )
+    )
+  },
+});
